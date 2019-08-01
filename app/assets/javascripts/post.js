f7c976f2095793b00ebd03196a8cb271a47253bd
vutuananh <vutuananhsama@gmail.com>
2019-08-01 13:00:16 +0700

    autofill

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,16 +1,18 @@
+$(function() {
+  return $('#post_book_name').autocomplete({
+    source: $('#post_book_name').data('autocomplete-source'),
+    select: function(event, ui) {
+      event.preventDefault();
+      $(this).val(ui.item.label);
+      $('#book_id').val(ui.item.value);
+    }
+  });
+});
+
 $(document).ready(function() {
   $('.select-book').select2({
     theme: 'bootstrap'
   });
-
-  if ($('textarea').length > 0) {
-    var data = $('.ckeditor');
-    $.each(data, function(i) {
-      CKEDITOR.replace(data[i].id)
-    });
-  }
-
-  CKEDITOR.config.height = 500;
 });
 
 // like
